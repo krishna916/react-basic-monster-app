41a8cc5265ecbc3835fda4425333167b4956db0c
krishnamurti <krishna916@outlook.com>
2020-05-02 20:03:02 +0530

    dynamic content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstien',
+          id: 'as'
+        },
+        {
+          name: 'Dracula',
+          id: 'asa'
+        },
+        {
+          name: 'Zombie',
+          id: 'asaa'
+        },
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))
+        }
+      </div>
+    );
+  }
 }
+
+  
 
 export default App;

53aa314bda8c5772b5f38b7da5b98b6194b1e937
krishnamurti <krishna916@outlook.com>
2020-05-03 23:46:42 +0530

    Updates

---
+++ precache-manifest.0152b00b35b7fd29de143fac8f57c60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101efd37e8aff15be31ee2b37f74e1b4",
+    "url": "/react-basic-monster-app/index.html"
+  },
+  {
+    "revision": "e69efee60e099b39f2ed",
+    "url": "/react-basic-monster-app/static/css/main.58132c7f.chunk.css"
+  },
+  {
+    "revision": "52ab36c179d2e860984b",
+    "url": "/react-basic-monster-app/static/js/2.1662debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-monster-app/static/js/2.1662debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69efee60e099b39f2ed",
+    "url": "/react-basic-monster-app/static/js/main.9c13829d.chunk.js"
+  },
+  {
+    "revision": "e69ec7d9914e118ee352",
+    "url": "/react-basic-monster-app/static/js/runtime-main.2e269868.js"
+  }
+]);
